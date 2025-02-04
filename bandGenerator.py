44d2c748cfaa86f529f0ac89718db1c3244dda3d
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-02-04 15:39:44 -0800

    first commit

---
+++ bandGenerator.py
@@ -0,0 +1,10 @@
+
+def main():
+
+    print("Welcome to the band name generator!")
+    cityName = input("What's the name of the city you grew up in?")
+    print(cityName)
+    petName = input("What's the name of your pet?")
+    print(petName)
+    print("Your band name could be " + cityName + " " + petName)
+main()
